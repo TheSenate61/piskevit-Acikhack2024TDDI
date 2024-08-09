c5d2bee6f3dac107bbeb4b7608657bf9eb93bdbc
TheSenate61 <81907811+TheSenate61@users.noreply.github.com>
2024-08-09 18:47:31 +0300

    Update Eval_Input_Retriever.py

--- Benchmark/Eval_Input_Retriever.py
+++ Benchmark/Eval_Input_Retriever.py
@@ -5,16 +5,13 @@ from transformers import TextStreamer
 import torch
 from datasets import Dataset
 
-# Adım 1: Parquet dosyalarını oku ve birleştir
-# `translated` klasöründeki tüm Parquet dosyalarını bulup, bunları birleştiriyoruz.
 path_to_files = '/content/drive/MyDrive/translated/*.parquet'
 parquet_files = glob.glob(path_to_files)
 df_list = [pd.read_parquet(file) for file in parquet_files]
 combined_df = pd.concat(df_list, ignore_index=True)
 questions_df = combined_df[['Question']]
 
-# Adım 2: Model ve tokenizer'ı yükle
-# KoçDigital LLM modelini yüklüyoruz ve modelin maksimum dizilim uzunluğunu belirtiyoruz.
+
 model, tokenizer = FastLanguageModel.from_pretrained(
     model_name="KOCDIGITAL/Kocdigital-LLM-8b-v0.1",
     max_seq_length=8196,
@@ -22,11 +19,9 @@ model, tokenizer = FastLanguageModel.from_pretrained(
     load_in_4bit=True,
 )
 
-# Yerel olarak 2x daha hızlı çıkarım yapmayı etkinleştir
 FastLanguageModel.for_inference(model)
 
-# Adım 3: Veri kümesini ön işleme tabi tut
-# `Question` sütununu işleyerek giriş olarak kullanılacak ve varsayılan bir cevap ekliyoruz.
+
 def preprocess_function(examples):
     return {
         "input": examples["Question"].strip(),
@@ -35,8 +30,7 @@ def preprocess_function(examples):
 
 processed_dataset = Dataset.from_pandas(questions_df).map(preprocess_function)
 
-# Adım 4: Çıktı oluşturma fonksiyonu
-# Her bir talimat için modelin cevap oluşturmasını sağlayan fonksiyonu tanımlıyoruz.
+
 def generate_output(instruction):
     alpaca_prompt = """Aşağıda, bir görevi tanımlayan bir talimat ile daha fazla bağlam sağlayan bir girdi bulunmaktadır. İsteği uygun ve etik kurallara uygun şekilde tamamlayan bir yanıt yazın.
 
@@ -53,8 +47,7 @@ def generate_output(instruction):
     outputs = model.generate(**inputs, streamer=text_streamer, max_new_tokens=128)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-# Adım 5: Her soru için çıktı oluşturma
-# Veri kümesindeki her bir talimat için çıktı oluşturup bunları bir listeye ekliyoruz.
+
 def generate_outputs_for_dataset(dataset):
     output_data = []
 
@@ -67,8 +60,7 @@ def generate_outputs_for_dataset(dataset):
 
 output_data = generate_outputs_for_dataset(processed_dataset)
 
-# Adım 6: Çıktıları bir CSV dosyasına kaydetme
-# Elde edilen çıktıları bir DataFrame'e dönüştürüp CSV dosyası olarak kaydediyoruz.
+
 output_df = pd.DataFrame(output_data)
 output_df.to_csv("/content/drive/MyDrive/generated_outputs_koç-UNSAFE.csv", index=False)
 
